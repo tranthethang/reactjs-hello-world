009c98cb824c3968a43c67ec3c7e1f0823b696cf
thangtt 6947 <thangtt6947@co-well.com.vn>
2019-01-23 19:50:23 +0900

    up

--- public/js/comment.js
+++ public/js/comment.js
@@ -1,13 +1,12 @@
 class Comment extends React.Component {
-    constructor(pros) {
-        super(pros);
+    constructor(props) {
+        super(props);
 
         this.state = {
-            avatar: pros.avatar,
-            author: pros.author,
-            comment: pros.comment
-        }
-
+            avatar: props.avatar,
+            author: props.author,
+            comment: props.children
+        };
     }
 
     render() {
@@ -17,7 +16,7 @@ class Comment extends React.Component {
                     <Avatar src={this.state.avatar}/>
                 </div>
                 <div>
-                    <Author name={this.state.author}/>
+                    <Author author={this.state.author}/>
                     <p className="comment__content">{this.state.comment}</p>
                 </div>
             </div>
@@ -26,12 +25,12 @@ class Comment extends React.Component {
 }
 
 class Avatar extends React.Component {
-    constructor(pros) {
-        super(pros);
+    constructor(props) {
+        super(props);
 
         this.state = {
-            src: pros.src
-        }
+            src: props.src
+        };
     }
 
     render() {
@@ -40,22 +39,23 @@ class Avatar extends React.Component {
 }
 
 class Author extends React.Component {
-    constructor(pros) {
-        super(pros);
+    constructor(props) {
+        super(props);
+
+        this.state = {
+            author: props.author
+        };
     }
 
     render() {
-        return <h3 className="comment__author">John Doe</h3>;
+        return <h5 className="comment__author">{this.state.author}</h5>;
     }
 }
 
 let element = <div>
-    <Comment avatar='https://place-hold.it/60x60?text=J&italic&bold' author='John Doe'
-             comment='This is comment of John Doe.'/>
-    <Comment avatar='https://place-hold.it/60x60?text=K&italic&bold' author='John Doe'
-             comment='This is comment of Kia Lee.'/>
-    <Comment avatar='https://place-hold.it/60x60?text=B&italic&bold' author='John Doe'
-             comment='This is comment of Best Big.'/>
+    <Comment avatar='https://place-hold.it/60x60?text=J&italic&bold' author='John Doe'>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec volutpat commodo consectetur. Praesent vel dignissim neque. Maecenas ut dapibus nunc. Donec volutpat libero augue, in varius urna blandit eget. Pellentesque fermentum, sem sit amet blandit faucibus, eros neque luctus diam, ut scelerisque risus quam id turpis. Ut tempus urna ut tellus tincidunt efficitur. Phasellus placerat id mi sed interdum. Phasellus porttitor tempor rhoncus. Vestibulum vulputate vel sapien at commodo. Maecenas eu faucibus nunc. Proin sodales, nibh a gravida bibendum, purus libero malesuada magna, ut rhoncus felis felis ac arcu. Pellentesque sit amet rhoncus justo, sed facilisis odio. Suspendisse potenti.</Comment>
+    <Comment avatar='https://place-hold.it/60x60?text=K&italic&bold' author='Kia Lee'>Morbi vitae augue elementum, faucibus leo sit amet, laoreet libero. Nunc egestas laoreet imperdiet. Donec gravida nisl risus, vel aliquam sem finibus at. Vivamus sodales libero quis tempor suscipit. Nam mollis, urna quis faucibus fermentum, ipsum justo porta tortor, varius pellentesque justo neque id turpis. Morbi auctor congue erat quis dapibus. Nam pretium odio ut erat vestibulum commodo. Aenean malesuada feugiat nulla a facilisis. Donec lacus lacus, sodales nec molestie non, scelerisque id magna. Morbi in felis convallis, lacinia mi suscipit, vulputate tortor. Vivamus ac ex nunc. Quisque condimentum tristique tellus ac placerat. Mauris vel dictum orci. Praesent sit amet aliquet ipsum, eu molestie tellus. Cras facilisis viverra velit, vel vehicula neque viverra quis.</Comment>
+    <Comment avatar='https://place-hold.it/60x60?text=B&italic&bold' author='Best Big'>Suspendisse at vulputate tellus. Donec sit amet aliquam odio. Vivamus vestibulum nulla eget neque dapibus volutpat. Maecenas mi risus, commodo vel accumsan in, laoreet in diam. Integer iaculis tempus arcu in cursus. Ut lobortis hendrerit bibendum. Nam id luctus risus, in euismod orci.</Comment>
 </div>;
 
 ReactDOM.render(
