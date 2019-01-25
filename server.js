f19666220b9a1d44f23200c755a9dd6075cc8164
thangtt 6947 <thangtt6947@co-well.com.vn>
2019-01-25 20:03:59 +0900

    up

--- server.js
+++ server.js
@@ -12,27 +12,21 @@ app.use(express.static(__dirname + '/node_modules/bootstrap/dist/'));
 app.get('/hello-world', (req, res) => {
     res.render('hello-world', {title: 'Hello World'})
 });
-
 app.get('/greeting', (req, res) => {
     res.render('greeting', {title: 'Greeting'})
 });
-
 app.get('/component', (req, res) => {
     res.render('component', {title: 'Components and Props'});
 });
-
 app.get('/comment', (req, res) => {
     res.render('comment', {title: 'Single comment'});
 });
-
 app.get('/clock', (req, res) => {
     res.render('clock', {title: 'State and Lifecycle'});
 });
-
 app.get('/event', (req, res) => {
     res.render('event', {title: 'Handling Events'});
 });
-
 app.get('/boil', (req, res) => {
     res.render('boil', {title: 'Boil calculator'})
 });
