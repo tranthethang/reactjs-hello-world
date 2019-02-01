828c54a07d6cb2653e6d258e3c046a16454bf756
thangtt 6947 <thangtt6947@co-well.com.vn>
2019-02-01 19:27:38 +0900

    up

--- public/js/todo-list.js
+++ public/js/todo-list.js
@@ -1,23 +1,65 @@
-class TodoForm extends React.Component {
+class TodoList extends React.Component {
     constructor(props) {
         super(props);
+
+        this.state = {
+            tasks: [1, 2, 3, 4, 5]
+        }
     }
 
     render() {
         return (
-            <form action='#' method='POST'>
-                <h3>Form</h3>
+            <div>
+                {this.state.tasks.map((index, task) => {
+                    return (
+                        <Task key={index} value={task}/>
+                    );
+                })}
+            </div>
+        );
+    }
+}
 
-                <p>
-                    <label>Task</label>
-                    <input/>
-                </p>
-            </form>
+class Task extends React.Component {
+    constructor(props) {
+        super(props);
+
+        this.state = {
+            isSleeping: true,
+            value: props.value
+        };
+
+        this.handleTransform = this.handleTransform.bind(this);
+        this.handleOnChange = this.handleOnChange.bind(this);
+    }
+
+    handleTransform() {
+        this.setState({
+            isSleeping: !this.state.isSleeping
+        });
+    }
+
+    handleOnChange(e) {
+        let newValue = e.target.value;
+
+        this.setState({
+            value: newValue
+        });
+    }
+
+    render() {
+        let content = this.state.isSleeping ? this.state.value :
+            <input type='text' defaultValue={this.state.value} onChange={this.handleOnChange}/>;
+
+        return (
+            <p onDoubleClick={this.handleTransform}>
+                {content}
+            </p>
         );
     }
 }
 
 ReactDOM.render(
-    <TodoForm/>,
+    <TodoList/>,
     document.getElementById('root')
 );
